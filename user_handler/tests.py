b5790fb272ac1d9b7cdf418756d94f3751ac1a42
Matteus Tanha <matteus.t@inflo.ai>
2019-11-22 17:59:10 +0000

    testing the login and registration

--- user_handler/tests.py
+++ user_handler/tests.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
 from .models import User, Organization
 
 
@@ -19,3 +23,24 @@ class TestUsers(TestCase):
         user = User.objects.get(name="foo")
         organization = Organization.objects.get(name="fooinc")
         self.assertEquals(organization.user.all()[0], user)
+
+
+class TestAPILogin(APITestCase):
+
+    def setUp(self):
+        user = User(name="foo", email="foo@bar.com", is_admin=True)
+        user.set_password("fooword")
+        user.save()
+        self.token, _ = Token.objects.get_or_create(user=user)
+
+    def test_login(self):
+        response = self.client.post('/users/login/', {'email': "foo@bar.com", "password": "fooword"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response)
+        self.assertEqual(response.data['token'], self.token.key)
+
+
+class TestAPIRegistration(APITestCase):
+
+    def test_registration(self):
+        response = self.client.post('/users/register/', {'email': "foo@bar.com", "password": "fooword", "name": "foo", "is_admin": True, "organization": "barinc"})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
