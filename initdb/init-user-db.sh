a46e003738d97b038e6257d10ebefbf1693cdb34
Matteus Tanha <matteus.t@inflo.ai>
2019-11-25 16:47:59 +0000

    fix indent error

--- initdb/init-user-db.sh
+++ initdb/init-user-db.sh
@@ -5,11 +5,11 @@ if psql -lqt | cut -d \| -f 1 | grep -qw $POSTGRES_DB; then
     echo "database already exists!"
 else
     # database does not exists
-    echo "creating databse and user for psql"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-      CREATE USER $POSTGRES_USER WITH ENCRYPTED PASSWORD '$POSTGRES_PASSWORD';
-      CREATE DATABASE $POSTGRES_DB;
-      GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
-    EOSQL
+echo "creating databse and user for psql"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER $POSTGRES_USER WITH ENCRYPTED PASSWORD '$POSTGRES_PASSWORD';
+  CREATE DATABASE $POSTGRES_DB;
+  GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+EOSQL
 
 fi
