44dbe909e04acd15f926b2b1f13ddb9f53359ed1
Matteus Tanha <matteus.t@inflo.ai>
2019-11-25 17:16:37 +0000

    added check_db

---
+++ check_db.py
@@ -0,0 +1,21 @@
+import socket
+import time
+import argparse
+import os
+
+""" Check if port is open, avoid docker-compose race condition """
+# arguments
+service_name = "postgreSQL"
+port = int(os.getenv("POSTGRES_PORT"))
+ip = os.getenv("POSTGRES_HOST")
+
+# Infinite loop
+while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((ip, port))
+    if result == 0:
+        print("{0} port is open! Bye!".format(service_name))
+        break
+    else:
+        print("{0} port is not open! I'll check it soon!".format(service_name))
+        time.sleep(3)
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Checking if database is ready
+echo "checking if database is ready"
+python check_db.py
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
