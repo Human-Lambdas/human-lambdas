4a403028b5169fe88c017b2349df306070ab0588
Matteus Tanha <matteus.t@inflo.ai>
2019-12-02 18:33:31 +0000

    only message

--- check_db.py
+++ check_db.py
@@ -9,12 +9,14 @@ port = int(os.getenv("POSTGRES_PORT"))
 ip = os.getenv("POSTGRES_HOST")
 
 # Infinite loop
-while True:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((ip, port))
-    if result == 0:
-        print("{0} port is open! Bye!".format(service_name))
-        break
-    else:
-        print("{0} port is not open! I'll check it soon!".format(service_name))
-        time.sleep(3)
+print("{0} port {1} is not open for host {2}".format(service_name, port, ip))
+
+# while True:
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     result = sock.connect_ex((ip, port))
+#     if result == 0:
+#         print("{0} port is open! Bye!".format(service_name))
+#         break
+#     else:
+#         print("{0} port is not open! I'll check it soon!".format(service_name))
+#         time.sleep(3)
