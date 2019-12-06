21f9a551a2867ff477e9db9ff2816f0b7ca15ca0
Matteus Tanha <matteus.t@inflo.ai>
2019-12-06 18:25:35 +0000

    styling

--- hl_rest_api/settings.py
+++ hl_rest_api/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "human-lambdas-api.eu-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = [
+    "0.0.0.0",
+    "localhost",
+    "human-lambdas-api.eu-west-2.elasticbeanstalk.com",
+]
 
 
 # Application definition
