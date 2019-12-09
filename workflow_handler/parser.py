1ed03c9d7a9c080180ae4fee73af8df7fa816bef
Sean Fahy <Seanfahy45@gmail.com>
2019-12-09 18:08:05 +0000

    added new parser function to create tasks based on a given workflow and task list, as well as instantiated the sample_workflow as a Workflow, also changing the for loop notation to dot instead of dictionary

--- workflow_handler/parser.py
+++ workflow_handler/parser.py
@@ -1,10 +1,11 @@
 import csv
 
+from .models import Task
 
 def validate_keys(csv_file, workflow):
     dataset = csv.reader(csv_file)
     title_row = next(dataset)
-    for input in workflow["inputs"]:
+    for input in workflow.inputs:
         value_count = title_row.count(input["key"])
         if value_count > 1:
             raise Exception("There are duplicate column names")
@@ -22,13 +23,27 @@ def format_csv(csv_file, workflow):
             continue
         else:
             task = []
-            for input in workflow["inputs"]:
+            for input in workflow.inputs:
                 task.append({
                     "key": input["key"],
                     "name": input["name"],
                     "format": input["format"],
-                    "value": row[title_row.index(input["key"])]
+                    "value": row[title_row.index(input["key"])],
+                    "workflow": workflow
                 })
 
         tasks.append(task)
     return tasks
+
+def create_tasks(tasks, workflow):
+    for task in tasks:
+        input_data=[]
+        for field in task:
+            if workflow == field["workflow"]:
+                input = {field["key"]: field["value"]}
+                input_data.append(input)
+            else:
+                raise Exception("The task's workflow does not match the workflow provided")
+        name="%s_task_%d" % (workflow.organization, workflow.id)
+        new_task = Task(name=name, workflow=workflow, input_data=input_data, output_data={})
+        new_task.save()
