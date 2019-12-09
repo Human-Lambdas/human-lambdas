6a75728114879dfecc121612354254e85ee2d2ef
Matteus Tanha <matteus.t@inflo.ai>
2019-12-09 10:37:59 +0000

    fix debug variable issue

--- hl_rest_api/settings.py
+++ hl_rest_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = bool(os.getenv("DEBUG", True))
 
 ALLOWED_HOSTS = [
     "0.0.0.0",
