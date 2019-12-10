c58095bfef013af5e36232651ed911afde28e647
MatteusT <matteus.tanha@gmail.com>
2019-12-10 11:37:12 +0000

    change static files directory

--- hl_rest_api/settings.py
+++ hl_rest_api/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = "user_handler.User"
 
