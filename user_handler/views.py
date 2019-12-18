73844bd2d5402cdfb393b4ad08c49be8302a0b77
Sean Fahy <Seanfahy45@gmail.com>
2019-12-18 00:57:25 +0000

    fixed another linter issue

--- user_handler/views.py
+++ user_handler/views.py
@@ -4,7 +4,6 @@ from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, Retrie
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 from .models import User, Organization
