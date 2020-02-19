ac33267e734c06557d9e750d9cfa6028d5ee861f
MatteusT <matteus.tanha@gmail.com>
2020-02-19 09:44:50 +0000

    no email and password needed to get api token

--- user_handler/serializers.py
+++ user_handler/serializers.py
@@ -1,7 +1,6 @@
 import logging
 
 from rest_framework import serializers
-from django.contrib.auth import authenticate
 
 from .models import User, Organization
 
@@ -64,29 +63,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class APITokenUserSerializer(serializers.Serializer):
-    email = serializers.CharField()
-    password = serializers.CharField()
-
     def validate(self, attrs):
-        email = attrs.get("email")
-        password = attrs.get("password")
-
-        if email and password:
-            user = authenticate(
-                request=self.context.get("request"), username=email, password=password
-            )
-
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
-            if not user:
-                msg = "Unable to log in with provided credentials."
-                raise serializers.ValidationError(msg, code="authorization")
-        else:
-            msg = 'Must include "email" and "password".'
+        request = self.context.get("request")
+        if not request.user:
+            msg = "Unable to log in with user."
             raise serializers.ValidationError(msg, code="authorization")
-
-        attrs["user"] = user
+        attrs["user"] = request.user
         return attrs
 
 
--- user_handler/views.py
+++ user_handler/views.py
@@ -173,7 +173,7 @@ class APIAuthToken(APIView):
     serializer_class = APITokenUserSerializer
     permission_classes = (IsAuthenticated,)
 
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         serializer = self.serializer_class(
             data=request.data, context={"request": request}
         )
