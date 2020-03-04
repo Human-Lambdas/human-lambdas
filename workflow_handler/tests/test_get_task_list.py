0e333734048a01999447ff6e4cf5bb5119b1bf97
MatteusT <matteus.tanha@gmail.com>
2020-03-04 13:26:26 +0000

    changed the path for getting completed task

--- workflow_handler/tests/test_get_task_list.py
+++ workflow_handler/tests/test_get_task_list.py
@@ -71,12 +71,12 @@ class TestTaskList(APITestCase):
 
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
         response = self.client.get(
-            "/v1/orgs/{}/workflows/{}/completed/".format(self.org_id, self.workflow_id)
+            "/v1/orgs/{}/workflows/{}/tasks/completed/".format(self.org_id, self.workflow_id)
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
         self.assertEqual(len(response.data["tasks"]), 100, response.data)
         response = self.client.get(
-            "/v1/orgs/{}/workflows/{}/completed/".format(self.org_id, self.workflow_id),
+            "/v1/orgs/{}/workflows/{}/tasks/completed/".format(self.org_id, self.workflow_id),
             format="json",
             data={"limit": 50},
         )
