b48a6089d4d0c83bbbcae4485f44baa255dccb95
Sean Fahy <seanfahy123>
2020-03-24 10:45:33 +0000

    changed match rules

--- user_handler/views.py
+++ user_handler/views.py
@@ -235,7 +235,8 @@ class SendInviteView(APIView):
         already_added_email_list = []
 
         for email in email_set:
-            if bool(re.fullmatch(r"\"?([-a-zA-Z0-9_.`?{}]+@\w+\.\w+)\"?", email)):
+            if bool(re.match(r"\"?([-a-zA-Z0-9_.`?{}]+@\w+\.\w+)\"?", email)):
+                print("YES:" + email)
                 # checks the email is valid
                 user = User.objects.filter(email=email).first()
                 organization = Organization.objects.get(pk=kwargs["org_id"])
