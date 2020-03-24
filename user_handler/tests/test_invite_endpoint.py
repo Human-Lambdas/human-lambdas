3bdb30185864a3d64c9148e55ce41f9ce33840eb
Sean Fahy <seanfahy123>
2020-03-24 10:56:36 +0000

    fixed error

--- user_handler/tests/test_invite_endpoint.py
+++ user_handler/tests/test_invite_endpoint.py
@@ -32,67 +32,67 @@ class TestInvite(APITestCase):
         self.org_id = organization.id
         organization.add_admin(user)
 
-    # def test_endpoint_call(self):
-    #     self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
-    #     response = self.client.post(
-    #         "/v1/orgs/{0}/invite".format(self.org_id),
-    #         {"emails": "lambda@sigma.com,alpha@beta.com,gamma@delta.com",},
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_endpoint_call(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        response = self.client.post(
+            "/v1/orgs/{0}/invite".format(self.org_id),
+            {"emails": "lambda@sigma.com,alpha@beta.com,gamma@delta.com",},
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_endpoint_call_with_spaces(self):
-    #     self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
-    #     response = self.client.post(
-    #         "/v1/orgs/{0}/invite".format(self.org_id),
-    #         {"emails": "lambda@sigma.com     ,alpha@beta.com,    gamma@delta.com    ",},
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_endpoint_call_with_spaces(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        response = self.client.post(
+            "/v1/orgs/{0}/invite".format(self.org_id),
+            {"emails": "lambda@sigma.com     ,alpha@beta.com,    gamma@delta.com    ",},
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_endpoint_call_duplicate_emails(self):
-    #     self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
-    #     response = self.client.post(
-    #         "/v1/orgs/{0}/invite".format(self.org_id),
-    #         {
-    #             "emails": "lambda@sigma.com,alpha@beta.com,gamma@delta.com,alpha@beta.com",
-    #         },
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_endpoint_call_duplicate_emails(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        response = self.client.post(
+            "/v1/orgs/{0}/invite".format(self.org_id),
+            {
+                "emails": "lambda@sigma.com,alpha@beta.com,gamma@delta.com,alpha@beta.com",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_endpoint_call_no_jwt(self):
-    #     response = self.client.post(
-    #         "/v1/orgs/{0}/invite".format(self.org_id),
-    #         {"emails": "lambda@sigma.com,alpha@beta.com,gamma@delta.com",},
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_endpoint_call_no_jwt(self):
+        response = self.client.post(
+            "/v1/orgs/{0}/invite".format(self.org_id),
+            {"emails": "lambda@sigma.com,alpha@beta.com,gamma@delta.com",},
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # def test_endpoint_call_already_added_email(self):
-    #     self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
-    #     response = self.client.post(
-    #         "/v1/orgs/{0}/invite".format(self.org_id),
-    #         {"emails": "lambda@sigma.com,foo@bar.com,gamma@delta.com",},
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_endpoint_call_already_added_email(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        response = self.client.post(
+            "/v1/orgs/{0}/invite".format(self.org_id),
+            {"emails": "lambda@sigma.com,foo@bar.com,gamma@delta.com",},
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_endpoint_call_invalid_emails(self):
-    #     self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
-    #     response = self.client.post(
-    #         "/v1/orgs/{0}/invite".format(self.org_id),
-    #         {"emails": "lambda@sigma.com,foo@foo,bar.com",},
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_endpoint_call_invalid_emails(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        response = self.client.post(
+            "/v1/orgs/{0}/invite".format(self.org_id),
+            {"emails": "lambda@sigma.com,foo@foo,bar.com",},
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_endpoint_call_invalid_emails_and_already_added_emails(self):
-    #     self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
-    #     response = self.client.post(
-    #         "/v1/orgs/{0}/invite".format(self.org_id),
-    #         {"emails": "lambda@sigma.com,foo@foo,bar.com,foo@bar.com",},
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_endpoint_call_invalid_emails_and_already_added_emails(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        response = self.client.post(
+            "/v1/orgs/{0}/invite".format(self.org_id),
+            {"emails": "lambda@sigma.com,foo@foo,bar.com,foo@bar.com",},
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_endpoint_call_email_with_underscore(self):
         self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
         response = self.client.post(
             "/v1/orgs/{0}/invite".format(self.org_id),
-            {"emails": "lam__bda@sigma.com,foo@foo,bar.com,foo@bar.com",},
+            {"emails": "lam__bda@sigma.com,foo@foobarinc.com",},
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
