0a0847d3e6bdb3f3bc35106764af79beaa340bce
Sean Fahy <seanfahy123>
2020-03-27 13:27:58 +0000

    removed redundant imports

--- user_handler/tests/test_utils.py
+++ user_handler/tests/test_utils.py
@@ -1,11 +1,8 @@
 from ..utils import is_invalid_email
 from rest_framework.test import APITestCase
-from rest_framework import status
-
-from user_handler.models import User, Organization
 
 
-class TestUserRoles(APITestCase):
+class TestUtils(APITestCase):
 
     def test_is_email_valid(self):
         self.assertTrue(is_invalid_email("test@test"))
