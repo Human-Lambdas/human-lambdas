9d4afba12db7b398dd8aa68e418f97158f859837
MatteusT <matteus.tanha@gmail.com>
2020-04-02 17:36:43 +0100

    custom jwt token

---
+++ user_handler/jwt_token.py
@@ -0,0 +1,19 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class HLTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["email"] = user.email
+        token["name"] = user.name
+        # ...
+
+        return token
+
+
+class HLTokenObtainPairView(TokenObtainPairView):
+    serializer_class = HLTokenObtainPairSerializer
