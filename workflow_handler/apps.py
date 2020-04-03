3abfaf445ed5ccb0c2813d5b9ebc86bac4029b60
Bernat Fages <bernat@bernats-mbp.home>
2020-04-03 16:02:42 +0100

    Migrating segment key to env variable

--- workflow_handler/apps.py
+++ workflow_handler/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 import analytics
 
@@ -7,4 +8,4 @@ class WorkflowHandlerConfig(AppConfig):
     name = "workflow_handler"
 
     def ready(self):
-        analytics.write_key = "WPaNpRkJK2VIxuIZ0GPHCpla2ch4hhM1"
+        analytics.write_key = settings.SEGMENT_KEY
