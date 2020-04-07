fd99f682690c282fbdf8a833d00f702ceeb8ee4a
Sean Fahy <sean@humanlambdas.com>
2020-04-07 17:12:41 +0200

    made small fixes

--- user_handler/admin.py
+++ user_handler/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import User, Organization, Invitation
+from .models import User, Organization, Invitation, ForgottenPassword
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Organization)
 admin.site.register(Invitation)
+admin.site.register(ForgottenPassword)
