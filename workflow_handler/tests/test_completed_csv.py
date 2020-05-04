75ff1fae8bee4ea7d3b95b87cfdf83c88fa8ff04
Sean Fahy <sean@humanlambdas.com>
2020-05-04 23:37:40 +0200

    added tests for when another output is given, as well as when an output is removed.

--- workflow_handler/tests/test_completed_csv.py
+++ workflow_handler/tests/test_completed_csv.py
@@ -150,33 +150,276 @@ class TestTaskCount(APITestCase):
         self.assertEqual(needed_csv, received_csv)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        #         updated_workflow_data = {
-        #     "outputs": [
-        #         {
-        #             "id": "tertiary_output",
-        #             "name": "terttert",
-        #             "type": "single-selection",
-        #             "single-selection": {
-        #                 "value": "TO1" if i % 2 == 0 else "TO2",
-        #                 "options": [
-        #                     {"id": "fooalpha2", "name": "TO1"},
-        #                     {"id": "barbeta2", "name": "TO2"},
-        #                 ],
-        #             },
-        #         },
-        #     ],
-        # }
+    def test_list_completed_task_internal_add_output(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        for i in range(self.total_rows - 1):
+            response = self.client.get(
+                "/v1/orgs/{0}/workflows/{1}/tasks/next".format(
+                    self.org_id, self.workflow_id
+                ),
+                format="json",
+            )
+            output_list = [
+                {
+                    "id": "foo",
+                    "name": "foo",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "value": "foo2" if i % 2 == 0 else "bar2",
+                        "options": [
+                            {"id": "foo2", "name": "foo2"},
+                            {"id": "bar2", "name": "bar2"},
+                        ],
+                    },
+                },
+                {
+                    "id": "secondary_output",
+                    "name": "barbar",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "value": "SO1" if i % 2 == 0 else "SO2",
+                        "options": [
+                            {"id": "foo2", "name": "SO1"},
+                            {"id": "bar2", "name": "SO2"},
+                        ],
+                    },
+                },
+            ]
+            task = response.data
+            response_data = {"inputs": task["inputs"], "outputs": output_list}
+            response = self.client.patch(
+                "/v1/orgs/{0}/workflows/{1}/tasks/{2}".format(
+                    self.org_id, self.workflow_id, task["id"]
+                ),
+                data=response_data,
+                format="json",
+            )
 
-        # response = self.client.patch(
-        #     "/v1/orgs/{0}/workflows/{1}".format(self.org_id, self.workflow_id),
-        #     updated_workflow_data,
-        #     format="json",
-        # )
-        # print(response.data)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
-        # print(Workflow.objects.get(pk=self.workflow_id))
+        updated_workflow_data = {
+            "outputs": [
+                {
+                    "id": "foo",
+                    "name": "foo",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "options": [
+                            {"id": "foo2", "name": "foo2"},
+                            {"id": "bar2", "name": "bar2"},
+                        ],
+                    },
+                },
+                {
+                    "id": "secondary_output",
+                    "name": "barbar",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "options": [
+                            {"id": "foo2", "name": "SO1"},
+                            {"id": "bar2", "name": "SO2"},
+                        ],
+                    },
+                },
+                {
+                    "id": "tertiary_output",
+                    "name": "terttert",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "options": [
+                            {"id": "fooalpha2", "name": "TO1"},
+                            {"id": "barbeta2", "name": "TO2"},
+                        ],
+                    },
+                },
+            ],
+        }
 
+        response = self.client.patch(
+            "/v1/orgs/{0}/workflows/{1}".format(self.org_id, self.workflow_id),
+            updated_workflow_data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
 
-# You need to test with adding another output, checking that new ones are applied
-# and old ones are left empty. Also try it with removing an output and testing that
-#  the tasks with that output included have that output ignored.
+        response = self.client.get(
+            "/v1/orgs/{0}/workflows/{1}/tasks/next".format(
+                self.org_id, self.workflow_id
+            ),
+            format="json",
+        )
+        output_list = [
+            {
+                "id": "foo",
+                "name": "foo",
+                "type": "single-selection",
+                "single-selection": {
+                    "value": "bar2",
+                    "options": [
+                        {"id": "foo2", "name": "foo2"},
+                        {"id": "bar2", "name": "bar2"},
+                    ],
+                },
+            },
+            {
+                "id": "secondary_output",
+                "name": "barbar",
+                "type": "single-selection",
+                "single-selection": {
+                    "value": "SO2",
+                    "options": [
+                        {"id": "foo2", "name": "SO1"},
+                        {"id": "bar2", "name": "SO2"},
+                    ],
+                },
+            },
+            {
+                "id": "tertiary_output",
+                "name": "terttert",
+                "type": "single-selection",
+                "single-selection": {
+                    "value": "TO2",
+                    "options": [
+                        {"id": "fooalpha2", "name": "TO1"},
+                        {"id": "barbeta2", "name": "TO2"},
+                    ],
+                },
+            },
+        ]
+        task = response.data
+        response_data = {"inputs": task["inputs"], "outputs": output_list}
+        response = self.client.patch(
+            "/v1/orgs/{0}/workflows/{1}/tasks/{2}".format(
+                self.org_id, self.workflow_id, task["id"]
+            ),
+            data=response_data,
+            format="json",
+        )
+        response = self.client.get(
+            "/v1/orgs/{}/workflows/{}/tasks/completed-tasks-csv".format(
+                self.org_id, self.workflow_id
+            )
+        )
+        needed_csv = repr(
+            """Alpha,Beta,Gamma,foo,secondary_output,tertiary_output
+7,8,9,bar2,SO2,TO2
+4,5,6,bar2,SO2,
+1,2,3,foo2,SO1,""".strip()
+        )
+        received_csv = repr(
+            response.getvalue().decode("utf-8").rstrip().replace("\r", "")
+        )
+        self.assertEqual(needed_csv, received_csv)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_completed_task_internal_remove_output(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.access_token)
+        for i in range(self.total_rows - 1):
+            response = self.client.get(
+                "/v1/orgs/{0}/workflows/{1}/tasks/next".format(
+                    self.org_id, self.workflow_id
+                ),
+                format="json",
+            )
+            output_list = [
+                {
+                    "id": "foo",
+                    "name": "foo",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "value": "foo2" if i % 2 == 0 else "bar2",
+                        "options": [
+                            {"id": "foo2", "name": "foo2"},
+                            {"id": "bar2", "name": "bar2"},
+                        ],
+                    },
+                },
+                {
+                    "id": "secondary_output",
+                    "name": "barbar",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "value": "SO1" if i % 2 == 0 else "SO2",
+                        "options": [
+                            {"id": "foo2", "name": "SO1"},
+                            {"id": "bar2", "name": "SO2"},
+                        ],
+                    },
+                },
+            ]
+            task = response.data
+            response_data = {"inputs": task["inputs"], "outputs": output_list}
+            response = self.client.patch(
+                "/v1/orgs/{0}/workflows/{1}/tasks/{2}".format(
+                    self.org_id, self.workflow_id, task["id"]
+                ),
+                data=response_data,
+                format="json",
+            )
+
+        updated_workflow_data = {
+            "outputs": [
+                {
+                    "id": "foo",
+                    "name": "foo",
+                    "type": "single-selection",
+                    "single-selection": {
+                        "options": [
+                            {"id": "foo2", "name": "foo2"},
+                            {"id": "bar2", "name": "bar2"},
+                        ],
+                    },
+                },
+            ],
+        }
+
+        response = self.client.patch(
+            "/v1/orgs/{0}/workflows/{1}".format(self.org_id, self.workflow_id),
+            updated_workflow_data,
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+
+        response = self.client.get(
+            "/v1/orgs/{0}/workflows/{1}/tasks/next".format(
+                self.org_id, self.workflow_id
+            ),
+            format="json",
+        )
+        output_list = [
+            {
+                "id": "foo",
+                "name": "foo",
+                "type": "single-selection",
+                "single-selection": {
+                    "value": "bar2",
+                    "options": [
+                        {"id": "foo2", "name": "foo2"},
+                        {"id": "bar2", "name": "bar2"},
+                    ],
+                },
+            },
+        ]
+        task = response.data
+        response_data = {"inputs": task["inputs"], "outputs": output_list}
+        response = self.client.patch(
+            "/v1/orgs/{0}/workflows/{1}/tasks/{2}".format(
+                self.org_id, self.workflow_id, task["id"]
+            ),
+            data=response_data,
+            format="json",
+        )
+        response = self.client.get(
+            "/v1/orgs/{}/workflows/{}/tasks/completed-tasks-csv".format(
+                self.org_id, self.workflow_id
+            )
+        )
+        needed_csv = repr(
+            """Alpha,Beta,Gamma,foo
+7,8,9,bar2
+4,5,6,bar2
+1,2,3,foo2""".strip()
+        )
+        received_csv = repr(
+            response.getvalue().decode("utf-8").rstrip().replace("\r", "")
+        )
+        self.assertEqual(needed_csv, received_csv)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
