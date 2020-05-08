973880ea6b5999aec580f3d730e3082a0bc39e3c
Sean Fahy <sean@humanlambdas.com>
2020-05-08 11:42:36 +0200

    added date as an option for validation

--- workflow_handler/schemas.py
+++ workflow_handler/schemas.py
@@ -6,7 +6,7 @@ WORKFLOW_INPUT_SCHEMA = Schema(
         {
             "id": And(str, len),
             "name": And(str, len),
-            "type": Or("text", "image", "number"),
+            "type": Or("text", "image", "number", "date"),
             Optional("layout"): dict,
         }
     ],
@@ -18,7 +18,7 @@ TASK_INPUT_SCHEMA = Schema(
         {
             "id": And(str, len),
             "name": And(str, len),
-            "type": Or("text", "image", "number"),
+            "type": Or("text", "image", "number", "date"),
             Optional("layout"): dict,
             "value": str,
         }
