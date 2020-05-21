941bf6ee0203da1a4a3835345a47d27f24ba7c22
Bernat Fages <bernatfp@gmail.com>
2020-05-21 11:47:06 +0100

    Moar attributes

--- user_handler/utils.py
+++ user_handler/utils.py
@@ -59,12 +59,22 @@ def register_events(user_obj, organization_obj, existing_org):
         },
     )
     if not existing_org:
-        analytics.track(user_obj.pk, "Org Created", {"org_id": organization_obj.pk})
+        analytics.track(
+            user_obj.pk,
+            "Org Created",
+            {
+                "org_id": organization_obj.pk,
+                "org_name": organization_obj.name,
+                "user_id": user_obj.pk,
+                "user_email": user_obj.email,
+            },
+        )
     analytics.track(
         user_obj.pk,
         "User Created",
         {
             "org_id": organization_obj.pk,
+            "org_name": organization_obj.name,
             "user_id": user_obj.pk,
             "user_email": user_obj.email,
         },
