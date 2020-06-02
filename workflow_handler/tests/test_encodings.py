cf0dc5727e42317c2666b89de0c11e86416a7d52
MatteusT <matteus.tanha@gmail.com>
2020-06-02 16:57:57 +0100

    minor change

--- workflow_handler/tests/test_encodings.py
+++ workflow_handler/tests/test_encodings.py
@@ -10,12 +10,12 @@ class TestEncodings(APITestCase):
     def setUp(self):
         self.raw_text = []
         self.expected = [
-            'premi?re is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n',
-            'première is first\npremière is slightly different\nКириллица is Cyrillic\n𐐀 am Deseret\n',
-            'premi\x8are is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n',
-            'première is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n',
-            'premi?re is first\npremie?re is slightly different\nëÉÒÉÌÌÉÃÁ is Cyrillic\n? am Deseret\n',
-            'première is first\npremière is slightly different\nКириллица is Cyrillic\n𐐀 am Deseret\n'
+            """premi?re is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n""",
+            """première is first\npremière is slightly different\nКириллица is Cyrillic\n𐐀 am Deseret\n""",
+            """premi\x8are is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n""",
+            """première is first\npremie?re is slightly different\n????????? is Cyrillic\n? am Deseret\n""",
+            """premi?re is first\npremie?re is slightly different\nëÉÒÉÌÌÉÃÁ is Cyrillic\n? am Deseret\n""",
+            """première is first\npremière is slightly different\nКириллица is Cyrillic\n𐐀 am Deseret\n""",
         ]
         for efile in os.listdir(os.path.join(DATA_PATH, "encodings")):
             with open(os.path.join(DATA_PATH, "encodings", efile), "rb") as f:
