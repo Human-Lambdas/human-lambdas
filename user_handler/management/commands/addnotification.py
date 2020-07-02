acb1ff92eb75647ae68224f640a34e12a073a9d6
MatteusT <matteus.tanha@gmail.com>
2020-07-02 17:35:11 +0100

    script updated to save workflownotifications

--- user_handler/management/commands/addnotification.py
+++ user_handler/management/commands/addnotification.py
@@ -14,19 +14,18 @@ class Command(BaseCommand):
                 notification.save()
                 user.notifications = notification
                 user.save()
-                for org in user.organization_set.all():
-                    for workflow in org.workflow_set.all():
+            for org in user.organization_set.all():
+                for workflow in org.workflow_set.all():
+                    if not WorkflowNotification.objects.filter(
+                        workflow=workflow, notification=user.notifications
+                    ).exists():
                         WorkflowNotification(
-                            workflow=workflow, enabled=True, notification=notification
-                        )
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        "Enabled all workflow notifications for user %s" % user.pk
-                    )
+                            workflow=workflow,
+                            enabled=True,
+                            notification=user.notifications,
+                        ).save()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Set-up all workflow notifications for user %s" % user.pk
                 )
-            else:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        "Notification alredy exists for user %s" % user.pk
-                    )
-                )
+            )
