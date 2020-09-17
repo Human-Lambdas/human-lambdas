28bfa1c5c151b85d7c2f040b9bb5ee8ee1b98982
MatteusT <matteus.tanha@gmail.com>
2020-09-17 15:42:19 +0100

    _id added to data item

--- workflow_handler/schemas.py
+++ workflow_handler/schemas.py
@@ -1,96 +1,5 @@
 from schema import Schema, And, Or, Optional
 
-
-WORKFLOW_INPUT_SCHEMA = Schema(
-    [
-        {
-            "id": And(str, len),
-            "name": And(str, len),
-            "type": Or("text", "image", "number", "date", "audio", "video", "list"),
-            Optional("layout"): dict,
-            Optional("subtype"): And(str, len),
-        }
-    ],
-    ignore_extra_keys=True,
-)
-
-TASK_INPUT_SCHEMA = Schema(
-    [
-        {
-            "id": And(str, len),
-            "name": And(str, len),
-            "type": Or("text", "image", "number", "date", "audio", "video", "list"),
-            Optional("layout"): dict,
-            Optional("subtype"): And(str, len),
-            "value": Or(str, int, float, list),
-        }
-    ],
-    ignore_extra_keys=True,
-)
-
-
-OUTPUT_SCHEMA = Schema(
-    [
-        {
-            "id": And(str, len),
-            "name": And(str, len),
-            "type": Or(
-                "binary",
-                "text",
-                "number",
-                "date",
-                "single_selection",
-                "multiple_selection",
-            ),
-            Optional(
-                Or(
-                    "binary",
-                    "text",
-                    "number",
-                    "date",
-                    "single_selection",
-                    "multiple_selection",
-                )
-            ): dict,
-            Optional("logicJump"): dict,
-        }
-    ],
-    ignore_extra_keys=True,
-)
-
-
-UPDATE_OUTPUT_SCHEMA = Schema(
-    [
-        {
-            "id": And(str, len),
-            "name": And(str, len),
-            "type": Or(
-                "binary",
-                "text",
-                "number",
-                "date",
-                "single_selection",
-                "multiple_selection",
-            ),
-            Optional(
-                Or(
-                    "binary",
-                    "text",
-                    "number",
-                    "date",
-                    "single_selection",
-                    "multiple_selection",
-                )
-            ): {
-                Optional("value"): Or(bool, And(str, len), And(list, len), float, int),
-                Optional("options"): list,
-            },
-            Optional("logicJump"): dict,
-        }
-    ],
-    ignore_extra_keys=True,
-)
-
 _DATA_TYPES = Or(
     "binary",
     "text",
@@ -108,6 +17,7 @@ DATA_SCHEMA = Schema(
     [
         {
             "id": Or(And(str, len), int),
+            Optional("_id"): Or(And(str, len), int),
             "name": And(str, len),
             "type": _DATA_TYPES,
             Optional("layout"): dict,
