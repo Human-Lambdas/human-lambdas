58065caa246bd892ad9b0efa030ab467ffc6e808
MatteusT <matteus.tanha@gmail.com>
2020-09-24 17:45:27 +0100

    data migration also fixes underscore conversion

--- workflow_handler/data_mapping.py
+++ workflow_handler/data_mapping.py
@@ -1,8 +1,50 @@
 import copy
 
-from schema import SchemaError
+from schema import SchemaError, Schema, And, Or, Optional
 
-from .schemas import WORKFLOW_INPUT_SCHEMA, OUTPUT_SCHEMA
+
+WORKFLOW_INPUT_SCHEMA = Schema(
+    [
+        {
+            "id": And(str, len),
+            "name": And(str, len),
+            "type": Or("text", "image", "number", "date", "audio", "video", "list"),
+            Optional("layout"): dict,
+            Optional("subtype"): And(str, len),
+        }
+    ],
+    ignore_extra_keys=True,
+)
+
+
+OUTPUT_SCHEMA = Schema(
+    [
+        {
+            "id": And(str, len),
+            "name": And(str, len),
+            "type": Or(
+                "binary",
+                "text",
+                "number",
+                "date",
+                "single-selection",
+                "multiple-selection",
+            ),
+            Optional(
+                Or(
+                    "binary",
+                    "text",
+                    "number",
+                    "date",
+                    "single-selection",
+                    "multiple-selection",
+                )
+            ): dict,
+            Optional("logicJump"): dict,
+        }
+    ],
+    ignore_extra_keys=True,
+)
 
 
 def input2data(input_data, is_task=True):
@@ -10,7 +52,7 @@ def input2data(input_data, is_task=True):
         WORKFLOW_INPUT_SCHEMA.validate(input_data)
     data = copy.deepcopy(input_data)
     data[input_data["type"]] = {
-        "read-only": True,
+        "read_only": True,
         "placeholder": None,
     }
     if is_task:
@@ -19,7 +61,8 @@ def input2data(input_data, is_task=True):
     if "subtype" in input_data:
         del data["subtype"]
         data[data["type"]]["subtype"] = input_data["subtype"]
-    return data
+    udata = underscore_conversion(data)
+    return udata
 
 
 def output2data(output_data, is_task=False):
@@ -28,11 +71,12 @@ def output2data(output_data, is_task=False):
     data = copy.deepcopy(output_data)
     if output_data["type"] not in output_data:
         data[output_data["type"]] = {}
-    data[output_data["type"]]["read-only"] = False
+    data[output_data["type"]]["read_only"] = False
     data[output_data["type"]]["placeholder"] = None
     if "value" not in data[output_data["type"]] and is_task:
         data[output_data["type"]]["value"] = None
-    return data
+    udata = underscore_conversion(data)
+    return udata
 
 
 def get_workflow_data(workflow):
@@ -56,12 +100,33 @@ def get_task_data(task):
     return task_data
 
 
+def underscore_conversion(data):
+    data["type"] = data["type"].replace("-", "_")
+    udata = {k.replace("-", "_"): v for k, v in data.items()}
+    return udata
+
+
+def data_underscore(data):
+    new_data = []
+    for idata in data:
+        new_data.append(underscore_conversion(idata))
+    return new_data
+
+
 def migrate_data(workflow):
     try:
-        workflow.data = get_workflow_data(workflow)
+        wf_data = get_workflow_data(workflow)
+        if wf_data:
+            workflow.data = wf_data
+        else:
+            workflow.data = data_underscore(workflow.data)
         workflow.save()
     except SchemaError:
         return
     for task in workflow.task_set.all():
-        task.data = get_task_data(task)
+        task_data = get_task_data(task)
+        if task_data:
+            task.data = task_data
+        else:
+            task.data = data_underscore(task.data)
         task.save()
