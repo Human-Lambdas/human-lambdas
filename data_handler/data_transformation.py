92ab646831ede3c0a3627dac9f29f87f48a40635
Bernat Fages <bernatfp@gmail.com>
2020-12-19 18:03:27 +0100

    More robust NER support

--- data_handler/data_transformation.py
+++ data_handler/data_transformation.py
@@ -15,7 +15,14 @@ def form_int2ext(int_data):
 
 def ner_int2ext(int_data):
     int_value = default_int2ext(int_data)
-    return {"text": int_value, "entities": int_data["entities"]}
+    entities = []
+    for entity in int_data["entities"]:
+        entity["category"] = entity["tag"]
+        del entity["tag"]
+        if "text" in entity: del entity["text"] 
+        if "color" in entity: del entity["color"]
+        entities.append(entity)
+    return {"text": int_value, "entities": entities}
 
 
 def default_int2ext(int_data):
@@ -63,8 +70,14 @@ def form_ext2int(task_data, request_data):
 
 def ner_ext2int(task_data, request_data):
     task_data[task_data["type"]]["value"] = request_data[task_data["id"]]["text"]
-    task_data[task_data["type"]]["entities"] = request_data[task_data["id"]]["entities"]
-
+    entities = []
+    for entity in request_data[task_data["id"]]["entities"]:
+        if "category" in entity:
+            entity["tag"] = entity["category"]
+            del entity["category"]
+        #if it doesn't have 'category' or 'start' or 'end' data validation will fail later on
+        entities.append(entity)
+    task_data[task_data["type"]]["entities"] = entities
 
 TRANSFORM_EXT2INT_STATES = {
     "list": list_ext2int,
--- data_handler/data_validation.py
+++ data_handler/data_validation.py
@@ -65,6 +65,16 @@ def validate_number(data):
         )
     data[data["type"]]["value"] = number_value
 
+def validate_named_entity_recognition(data):
+    if not isinstance(data[data["type"]].get("value"), str):
+        raise DataValidationError(f"Data item with id {data['id']} is missing 'value' or not a string.")
+    if not isinstance(data[data["type"]].get("entities"), list):
+        raise DataValidationError(f"Data item with id {data['id']} is missing 'entities' or not a list.")
+    if not isinstance(data[data["type"]].get("options"), list):
+        raise DataValidationError(f"Data item with id {data['id']} is missing 'options' or not a list.")
+    for entity in data[data["type"]].get("entities"):
+        if not isinstance(entity.get("start"), int) or not isinstance(entity.get("end"), int) or not isinstance(entity.get("tag"), str):
+            raise DataValidationError(f"Entity missing one or more properties.")
 
 VALIDATION_STATES = {
     "single_selection": validate_single_selection,
@@ -72,6 +82,7 @@ VALIDATION_STATES = {
     "form_sequence": validate_form,
     "list": validate_list,
     "number": validate_number,
+    "named_entity_recognition": validate_named_entity_recognition,
 }
 
 
