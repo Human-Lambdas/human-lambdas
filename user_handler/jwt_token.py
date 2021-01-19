58b72d943777f37cbfc8f871f7a9cea6bdb8300b
alex-treebeard <alex@treebeard.io>
2021-01-19 12:02:51 +0000

    Add googl auth

--- user_handler/jwt_token.py
+++ user_handler/jwt_token.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from rest_framework import response
+from rest_framework.generics import CreateAPIView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from hl_rest_api import analytics
+from user_handler.models import User
 
 
 class HLTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -16,3 +20,31 @@ class HLTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class HLTokenObtainPairView(TokenObtainPairView):
     serializer_class = HLTokenObtainPairSerializer
+
+
+from google.auth.transport import requests
+from google.oauth2 import id_token
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class GoogleToken(CreateAPIView):
+    def create(self, request):
+        try:
+            token = request.data["token"]
+            idinfo = id_token.verify_oauth2_token(
+                token, requests.Request(), settings.GOOGLE_OAUTH_CLIENT_ID
+            )
+        except ValueError:
+            # Invalid token
+            pass
+        email = idinfo["email"]
+        user = User.objects.get(email=email)
+        refresh = RefreshToken.for_user(user)
+
+        return response.Response(
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=200,
+        )
