9caa4fbeec4b28b820be1bf719e459b504792ce0
alex-treebeard <alex@treebeard.io>
2021-03-03 10:46:01 +0000

    gcs integ

--- workflow_handler/r13n.py
+++ workflow_handler/r13n.py
@@ -1,54 +1,50 @@
+import json
 import logging
 from enum import Enum
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
-from django.utils import timezone
+from google.api_core.exceptions import NotFound
 from google.cloud import storage
 
 
 class Region(Enum):
+    bucket: str
+
     def __new__(cls, bucket: str):
         obj = object.__new__(cls)
         obj.bucket = bucket
         return obj
 
-    EU = "eu-data-stg"
+    EU = ""
     AU = "au-data-stg"
     US = "us-data-stg"
 
 
 logger = logging.getLogger(__file__)
 
-buckets = ["us-data-stg", "eu-data-stg", "au-data-stg"]
 
+def store(pk: int, region: Region, data: Union[None, Dict[Any, Any]]) -> None:
+    if region == Region.EU:
+        raise NotImplementedError("EU data should not use buckets")
 
-def store(pk: int, region: Region, data: Any) -> None:
     client = storage.Client()
 
-    for b in buckets:
-        logger.warning(f"{timezone.now()} get_bucket")
-        bucket = client.get_bucket(b)
-        logger.warning(f"{timezone.now()} done")
-
-        logger.warning(f"{timezone.now()} store blob")
-        blob = bucket.blob("test.txt")
-        logger.warning(f"{timezone.now()} upload_from_string")
-        blob.upload_from_string(f"written {timezone.now()}")
-        logger.warning(f"{timezone.now()} done {b}")
-    return None
+    bucket = client.get_bucket(region.bucket)
+    blob = bucket.blob(f"{pk}")
+    blob.upload_from_string(json.dumps(data))
 
 
-def retrieve(pk: int, region: Region) -> Dict[Any, Any]:
+def retrieve(pk: int, region: Region) -> Union[None, Dict[Any, Any]]:
+    if region == Region.EU:
+        raise NotImplementedError("EU data should not use buckets")
+
     client = storage.Client()
 
-    for b in buckets:
-        logger.warning(f"{timezone.now()} get_bucket")
-        bucket = client.get_bucket(b)
-        logger.warning(f"{timezone.now()} done")
-
-        logger.warning(f"{timezone.now()} retrieve blob")
-        blob = bucket.blob("test.txt")
-        logger.warning(f"{timezone.now()} download_as_text")
-        logger.warning(f"{timezone.now()} text: {blob.download_as_text()}")
-        logger.warning(f"{timezone.now()} done {b}")
-    return {}
+    bucket = client.get_bucket(region.bucket)
+    blob = bucket.blob(f"{pk}")
+    try:
+        text = blob.download_as_text()
+        return json.loads(text)
+    except NotFound:
+        logger.warning(f"Task {pk} not found in cloud bucket {region.bucket}.")
+        return None
---
+++ workflow_handler/tests/test_regional_storage.py
@@ -0,0 +1,25 @@
+import pytest
+
+from workflow_handler.r13n import Region, retrieve, store
+
+DATA = {"BL": 42}
+
+PK = 1
+
+
+class TestRegionalStorage:
+    def test_when_store_for_eu_then_exception(self):
+        with pytest.raises(NotImplementedError):
+            store(PK, Region.EU, {})
+
+    def test_when_retrieve_for_eu_then_exception(self):
+        with pytest.raises(NotImplementedError):
+            retrieve(PK, Region.EU)
+
+    def test_when_stored_then_can_be_fetched(self):
+        store(PK, Region.AU, DATA)
+
+        assert retrieve(PK, Region.AU) == DATA
+
+    def test_when_retrieving_unknown_pk_then_none(self):
+        assert retrieve(9876, Region.AU) is None
