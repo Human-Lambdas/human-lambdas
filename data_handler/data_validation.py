50c575848812b7dd81778b2509fc7cecd0fbf958
bernatfp <bernatfp@gmail.com>
2021-04-19 21:21:26 +0100

    Uncomment

--- data_handler/data_validation.py
+++ data_handler/data_validation.py
@@ -262,8 +262,7 @@ def validate_date(data, is_workflow):
         v = data[data["type"]].get(k)
         if not isinstance(v, (str, type(None))):
             raise DataValidationError(
-                # f"Data item with id {data['id']} is not a valid ISO-8601 string."
-                f"Data: {data}"
+                f"Data item with id {data['id']} is not a valid ISO-8601 string."
             )
 
         try:
@@ -271,8 +270,7 @@ def validate_date(data, is_workflow):
                 pass
         except:
             raise DataValidationError(
-                # f"Data item with id {data['id']} is not a valid ISO-8601 string."
-                f"Data: {data}"
+                f"Data item with id {data['id']} is not a valid ISO-8601 string."
             )
 
 
