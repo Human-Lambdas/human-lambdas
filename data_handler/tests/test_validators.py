39db42dc25171b7dfdcc6a176bd75b5a816bfe7e
alex-treebeard <alex@treebeard.io>
2021-04-28 15:03:20 +0100

    Use factory boy for validation tests

--- data_handler/tests/test_validators.py
+++ data_handler/tests/test_validators.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict
+
+import factory
 import pytest
 
 from data_handler.data_validation import DataValidationError, data_validation
@@ -5,71 +8,72 @@ from data_handler.data_validation import DataValidationError, data_validation
 VALID_URL = "http://localhost.com/workflows/10/edit.png"
 INVALID_URL = "http:/"
 
-LAYOUT = {
-    "h": 2,
-    "i": "6c9d6441-5be5-4a15-814c-1c9413e8dd22",
-    "minH": 2,
-    "minW": 4,
-    "moved": False,
-    "static": False,
-    "w": 5,
-    "x": 7,
-    "y": 0,
-}
+
+class BlockFactory(factory.Factory):
+    _id = "6c9d6441-5be5-4a15-814c-1c9413e8dd22"
+    id = "email_id"
+    layout = factory.Dict(
+        {
+            "h": 2,
+            "i": "6c9d6441-5be5-4a15-814c-1c9413e8dd22",
+            "minH": 2,
+            "minW": 4,
+            "moved": False,
+            "static": False,
+            "w": 5,
+            "x": 7,
+            "y": 0,
+        }
+    )
+    name = "Email Block Title"
+    type = "email"
 
 
 def create_email_block(value):
-    block = {
-        "_id": "6c9d6441-5be5-4a15-814c-1c9413e8dd22",
-        "email": {
-            "placeholder": "adf@asdf.co",
-            "read_only": False,
-            "use_placeholder": True,
-        },
-        "id": "email_id",
-        "layout": LAYOUT,
-        "name": "Email Block Title",
-        "type": "email",
+    email: Dict[str, Any] = {
+        "placeholder": "adf@asdf.co",
+        "read_only": False,
+        "use_placeholder": True,
     }
     if value is not None:
-        block["email"]["value"] = value
+        email["value"] = value
+
+    block = factory.build(dict, FACTORY_CLASS=BlockFactory, email=email, type="email")
     return block
 
 
 def create_email_form_block(value):
-    block = {
-        "_id": "74f041cc-c30a-49fb-9970-c1895cb79b59",
-        "form_sequence": {
-            "data": [
-                {
-                    "email": {},
-                    "id": "email_id",
-                    "name": "Email Question",
-                    "type": "email",
-                }
-            ],
-            "history": [],
-            "is_required": True,
-        },
-        "id": "form_sequence_id",
-        "layout": LAYOUT,
-        "name": "Form Sequence Block Title",
-        "type": "form_sequence",
+    form_sequence = {
+        "data": [
+            {
+                "email": {},
+                "id": "email_id",
+                "name": "Email Question",
+                "type": "email",
+            }
+        ],
+        "history": [],
+        "is_required": True,
     }
     if value is not None:
-        block["form_sequence"]["data"][0]["email"]["value"] = value
+        form_sequence["data"][0]["email"]["value"] = value
+
+    block = factory.build(
+        dict,
+        FACTORY_CLASS=BlockFactory,
+        form_sequence=form_sequence,
+        type="form_sequence",
+    )
     return block
 
 
 def create_date_block(value=None, placeholder=None):
-    block = {
-        "_id": "6b4d9cfc-998e-4cfd-8686-56119011e72f",
-        "date": {"value": value, "placeholder": placeholder},
-        "id": "date_id",
-        "layout": LAYOUT,
-        "name": "Date Block Title",
-        "type": "date",
-    }
+    block = factory.build(
+        dict,
+        FACTORY_CLASS=BlockFactory,
+        date={"value": value, "placeholder": placeholder},
+        type="date",
+    )
     return block
 
 
@@ -137,15 +141,9 @@ class TestValidators:
 
     @pytest.mark.parametrize("field, url, type", get_url_test())
     def test_url_validation(self, field, url, type):
-        block = {
-            "_id": "74f041cc-c30a-49fb-9970-c1895cb79b59",
-            type: {},
-            "id": "id",
-            "layout": LAYOUT,
-            "name": "asdf",
-            "type": type,
-        }
-
+        block = factory.build(
+            dict, FACTORY_CLASS=BlockFactory, **{type: {}, "type": type}
+        )
         block[type][field] = url
 
         if type == "bounding_boxes":
@@ -160,20 +158,19 @@ class TestValidators:
             data_validation([block])
 
     def test_when_empty_bounding_boxes_workflow_then_pass(self):
-        block = {
-            "_id": "c97e6561-fe75-42f9-b5e3-caf47521b789",
-            "bounding_boxes": {
-                "options": [
-                    {"id": "option_1", "name": "Option 1"},
-                    {"id": "option_2", "name": "Option 2"},
-                ],
-                "placeholder": "",
-            },
-            "id": "bounding_boxes_id",
-            "layout": LAYOUT,
-            "name": "Bounding Boxes Block Title",
-            "type": "bounding_boxes",
+        bounding_boxes = {
+            "options": [
+                {"id": "option_1", "name": "Option 1"},
+                {"id": "option_2", "name": "Option 2"},
+            ],
+            "placeholder": "",
         }
+        block = factory.build(
+            dict,
+            FACTORY_CLASS=BlockFactory,
+            bounding_boxes=bounding_boxes,
+            type="bounding_boxes",
+        )
 
         data_validation([block], is_workflow=True)
 
@@ -209,10 +206,7 @@ class TestValidators:
             pass
 
     def test_when_date_block_has_no_content_then_pass(self):
-        block = {
-            "id": "Date",
-            "name": "Date",
-            "type": "date",
-            "date": {"read_only": True},
-        }
+        block = factory.build(
+            dict, FACTORY_CLASS=BlockFactory, date={"read_only": True}, type="date"
+        )
         data_validation([block])
